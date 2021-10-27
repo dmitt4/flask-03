97e0775a53eafcfb1ac6325e709db7cfc55af0ab
timofeev <timofeev@fgosniias.ru>
2021-10-27 09:26:14 +0300

    Send to verification

--- project/forms.py
+++ project/forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.fields.core import SelectField, IntegerField
 from wtforms.fields.simple import PasswordField
 from wtforms.validators import DataRequired, Email, InputRequired, Length
+
+class CalcForm(FlaskForm):
+    x =  IntegerField("X", validators=[DataRequired()], render_kw={'class': 'form-control'})
+    oper = SelectField("oper", validators=[DataRequired()], choices=[('+', '+'), ('-', '-'), ('*', '*'), ('/', ':'), ('**', '**')], render_kw={'class': 'form-control'})
+    y = IntegerField("Y", validators=[DataRequired()], render_kw={'class': 'form-control'})
+    submit = SubmitField("ОК")
 
 class MessageForm(FlaskForm):
     name = StringField("Name: ", validators=[DataRequired()], render_kw={'class': 'form-control'})
