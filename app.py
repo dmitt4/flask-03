9d0f20ffcdb752687606d1e58f255b280f5f66af
timofeev <timofeev@fgosniias.ru>
2021-10-27 09:23:42 +0300

    Send to verification

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-from logging import debug
-from os import abort
 from flask import Flask, render_template, request
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 from config import Config
-from project.forms import MessageForm
+from project.forms import CalcForm
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = CalcForm()
 
-@app.route('/calc', methods = ['POST'])
-def calc():
+    if form.validate_on_submit():
+        x = form.x.data
+        oper = form.oper.data
+        y = form.y.data
+
+        return redirect(url_for('calc', x=x, oper=oper, y = y))
+    
+    return render_template('index.html', form=form)
+
+
+@app.route('/calc/<int:x>/<oper>/<int:y>/', methods = ['GET'])
+def calc(x: int, oper, y: int):
     operations = {
         '+': lambda x, y: x + y,
-        '/': lambda x, y: x / y,
+        ':': lambda x, y: x / y,
         '-': lambda x, y: x - y,
         '*': lambda x, y: x * y,
         '**': lambda x, y: x ** y}     
 
-    x = request.form.get('X', type=float)
-    oper = request.form.get('oper', type=str)
-    y = request.form.get('Y', type=float)
-
-    result_aquired = (oper in operations) and not (oper == '/' and y == 0 )
+    result_aquired = (oper in operations) and not (oper == ':' and y == 0 )
     result = None
     if result_aquired:
         result = operations[oper](x,y) 
@@ -36,19 +40,6 @@ def calc():
                             result = result
     )
 
-@app.route('/message/', methods=['get', 'post'])
-def message():
-    form = MessageForm()
-
-    if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        message = form.message.data
-        print(f'{name} {email} {message}')
-        print('\nData recieved. Now redirecting...')
-        return redirect(url_for('message'))
-    
-    return render_template('message.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='5000', debug=True) 
